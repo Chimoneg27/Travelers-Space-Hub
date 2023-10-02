e368bc89a66e8d95d0a5d83dfe35765ab1eef692
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-10-02 19:04:12 +0200

    Add rocket reducer to store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { configureStore } from '@reduxjs/toolkit';
+import rocketReducer from './Rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
+    rockets: rocketReducer,
   },
 });
 export default store;
