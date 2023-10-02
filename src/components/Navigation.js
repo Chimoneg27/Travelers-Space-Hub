a1679576bca5f7dbb1137488914b9b44b9d4ebfa
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-10-02 15:22:08 +0200

    Make required changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../Styles/Navigation.css';
 
 const Navigation = () => (
   <nav className="navigation">
     <h2>Logo</h2>
     <ul className="items">
-      <li><NavLink to="/">Rockets</NavLink></li>
-      <li><NavLink to="/Missions">Missions</NavLink></li>
-      <li><NavLink to="/My Profile">My Profile</NavLink></li>
+      <li><NavLink to="/" activeclassname="active-link" className="noactive-link">Rockets</NavLink></li>
+      <li><NavLink to="/Missions" activeclassname="active-link" className="noactive-link">Missions</NavLink></li>
+      <li><NavLink to="/My Profile" activeclassname="active-link" className="noactive-link">My Profile</NavLink></li>
     </ul>
   </nav>
 );
