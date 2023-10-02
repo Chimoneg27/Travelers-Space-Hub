2b7c9e7737d5352b0feda5a0c6183172a2895360
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-10-02 23:42:23 +0300

    Get Missions Data use api

---
+++ src/Redux/Missions/missionsSlice.js
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const apiLink = 'https://api.spacexdata.com/v3/missions';
+
+export const getMissionsData = createAsyncThunk('missions/getMissionsData', async (thunkAPI) => {
+  try {
+    const response = await axios.get(apiLink);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState: {
+    missionsArr: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getMissionsData.pending, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(getMissionsData.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.rocketArr = action.payload.map((mission) => ({
+          mission_id: mission.mission_id,
+          mission_name: mission.mission_name,
+          description: mission.description,
+        }));
+      })
+      .addCase(getMissionsData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error;
+      });
+  },
+});
+
+export default missionsSlice.reducer;
