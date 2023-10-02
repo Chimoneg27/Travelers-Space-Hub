0d898487c055f4a520722150bbcc0ad1fcd5985f
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-10-02 12:02:07 +0200

    Setup navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/My Profile" element={<MyProfile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
