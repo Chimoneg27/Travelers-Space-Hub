e0d8e6704abbcba781d969a24e543c4de7bdcc4e
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-10-04 09:52:42 +0300

    Create MissionProfile.js

---
+++ src/components/MissionProfile.js
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux';
+
+const MissionProfile = () => {
+  const { missions } = useSelector((state) => state.missions);
+  console.log(missions);
+  const missionsJoin = missions.filter((mission) => mission.mission_join === true);
+  console.log(missionsJoin);
+  return (
+    <>
+      {
+
+missionsJoin.map((mission) => (
+  <div
+    key={mission.mission_id}
+    className="mission-name-profile"
+  >
+    <h3>{mission.mission_name}</h3>
+  </div>
+))
+}
+    </>
+  );
+};
+export default MissionProfile;
